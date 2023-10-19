7c4a21e87e1656a3e3466be69a3e9d0bc89fbc13
Shubham <shubhampj@gmail.com>
2023-10-19 20:01:09 +0530

    Merge all APIs in one file

--- apiGetRequestsUser.py
+++ apiGetRequestsUser.py
@@ -17,48 +17,48 @@ if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
 # ----------------------------------------------------------------- check_email_address_availability
-@app.route('/api/check_email_address_availability', methods=['GET'])
-def api_check_email_address_availability():
-    print('Enter into API check_email_address_availability.')
-    with sqlite3.connect('fresh_keeper') as conn:
-                cursor = conn.cursor()
-    email_address = request.args.get('emailAddress')
-    print('Parameters Recieved.', email_address)
-    print('Calling db_check_email_availability')
-    db_check_email_availability_value = db_check_email_availability(cursor, email_address)
-    print('Return from db_check_email_availability with value: ', db_check_email_availability_value)
-    conn.commit()
-    conn.close()  
-    return jsonify({"isEmailAddress": db_check_email_availability_value}), 200 
+# @app.route('/api/check_email_address_availability', methods=['GET'])
+# def api_check_email_address_availability():
+#     print('Enter into API check_email_address_availability.')
+#     with sqlite3.connect('fresh_keeper') as conn:
+#                 cursor = conn.cursor()
+#     email_address = request.args.get('emailAddress')
+#     print('Parameters Recieved.', email_address)
+#     print('Calling db_check_email_availability')
+#     db_check_email_availability_value = db_check_email_availability(cursor, email_address)
+#     print('Return from db_check_email_availability with value: ', db_check_email_availability_value)
+#     conn.commit()
+#     conn.close()  
+#     return jsonify({"isEmailAddress": db_check_email_availability_value}), 200 
 
-# ----------------------------------------------------------------- check_email_address_availability
-@app.route('/api/check_phone_number_availability', methods=['GET'])
-def api_check_phone_number_availability():
-    print('Enter into API check_phone_number_availability.')
-    with sqlite3.connect('fresh_keeper') as conn:
-                cursor = conn.cursor()
-    phone_number = request.args.get('phoneNumber')
-    print('Parameters Recieved.', phone_number)
-    print('Calling db_phone_number_availability')
-    db_check_phone_number_availability_value = db_check_phone_number_availability(cursor, phone_number)
-    print('Return from db_check_email_availability with value: ', db_check_phone_number_availability_value)
-    conn.commit()
-    conn.close()  
-    return jsonify({"isPhoneNumber": db_check_phone_number_availability_value}), 200 
+# # ----------------------------------------------------------------- check_email_address_availability
+# @app.route('/api/check_phone_number_availability', methods=['GET'])
+# def api_check_phone_number_availability():
+#     print('Enter into API check_phone_number_availability.')
+#     with sqlite3.connect('fresh_keeper') as conn:
+#                 cursor = conn.cursor()
+#     phone_number = request.args.get('phoneNumber')
+#     print('Parameters Recieved.', phone_number)
+#     print('Calling db_phone_number_availability')
+#     db_check_phone_number_availability_value = db_check_phone_number_availability(cursor, phone_number)
+#     print('Return from db_check_email_availability with value: ', db_check_phone_number_availability_value)
+#     conn.commit()
+#     conn.close()  
+#     return jsonify({"isPhoneNumber": db_check_phone_number_availability_value}), 200 
 
-# -----------------------------------------------------------------
-@app.route('/api/get_and_send_email_passcode', methods=['GET'])
-def api_get_and_send_email_passcode():
-    print('Enter into API get_and_send_email_passcode.')
-    email_address = request.args.get('emailAddress')
-    passcode = random.randint(1000, 9999)
-    print('Passcode: ', passcode, 'for', email_address)
-    subject = 'Verification Code for Fresh Keeper'
-    body = 'Your verification code is: ' + passcode + '.'
-    print('Calling SMTP_send_email')
-    SMTP_send_email(email_address, subject, body)
-    print('Returned form SMTP_send_email')
-    return jsonify({"passcode": passcode}), 200 
+# # ----------------------------------------------------------------- get_and_send_email_passcode
+# @app.route('/api/get_and_send_email_passcode', methods=['GET'])
+# def api_get_and_send_email_passcode():
+#     print('Enter into API get_and_send_email_passcode.')
+#     email_address = request.args.get('emailAddress')
+#     passcode = random.randint(1000, 9999)
+#     print('Passcode: ', passcode, 'for', email_address)
+#     subject = 'Verification Code for Fresh Keeper'
+#     body = 'Your verification code is: ' + passcode + '.'
+#     print('Calling SMTP_send_email')
+#     SMTP_send_email(email_address, subject, body)
+#     print('Returned form SMTP_send_email')
+#     return jsonify({"passcode": passcode}), 200 
 
 # -----------------------------------------------------------------
 if __name__ == '__main__':
